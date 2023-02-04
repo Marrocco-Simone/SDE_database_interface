07053cd2a52becb70a7918f13abd9dcb0377267d
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-02-04 16:32:25 +0100

    login and basic apis

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { faker } = require("@faker-js/faker");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 mongoose.set("strictQuery", true);
 
@@ -24,36 +25,111 @@ const Content = mongoose.model("Content", ContentSchema);
 const app = express();
 const port = 3000;
 const mongoDbUrl = process.env.MONGODBURL;
+const jwtSecret = process.env.JWTSECRET;
 
 app.use(cors({ origin: "*" }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ success: true });
 });
 
-app.get("/newuser", async (req, res) => {
+app.get("/db", (req, res) => {
+  res.json({ success: true });
+});
+
+app.get("/db/newuser", async (req, res) => {
   const newUser = new User({
     email: faker.internet.email(),
-    password: faker.internet.password("memorable"),
+    password: faker.internet.password(),
   });
   await newUser.save();
 
   res.json(newUser);
 });
 
-app.get("/fakecontent", (req,res) => {
+app.get("/db/fakecontent", (req, res) => {
   const fakeContent = {
     content_text: faker.lorem.paragraph(),
     title: faker.animal.cat(),
-  }
-  faker.image.animals
+  };
+  faker.image.animals;
   res.json(fakeContent);
 });
 
-app.post("/login", async (req, res) => {});
-app.put("/generate", async (req, res) => {});
-app.get("/get", async (req, res) => {});
-app.post("/modify/:content_id", async (req, res) => {});
+app.post("/db/login", async (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ error: "Please send a valid request body" });
+  }
+
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ error: "Please send an email and a password" });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(401).json({ error: "Email not found" });
+  }
+
+  if (password !== user.password) {
+    return res.status(401).json({ error: "Password not correct" });
+  }
+
+  const token = jwt.sign({ _id: user._id.toString(), email }, jwtSecret, {
+    expiresIn: "2d",
+  });
+
+  res.json({ email, token });
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.status(401).json({ err: "No Token given" });
+
+  jwt.verify(token, jwtSecret, (err, user) => {
+    if (err) {
+      return res.status(403).json({ err: "Invalid Token" });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+app.put("/db/generate", authenticateToken, async (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ error: "Please send a valid request body" });
+  }
+
+  const { title, content_text } = req.body;
+
+  if (!title || !content_text) {
+    return res
+      .status(400)
+      .json({ error: "Please send all the required fields" });
+  }
+
+  const newContent = new Content({
+    title,
+    content_text,
+    user: req.user._id,
+    created_at: new Date(),
+  });
+  await newContent.save();
+
+  res.json(newContent);
+});
+
+app.get("/db/get", authenticateToken, async (req, res) => {
+  const userContents = await Content.find({ user: req.user._id });
+  res.json(userContents);
+});
+
+app.post("/db/modify/:content_id", authenticateToken, async (req, res) => {});
 
 app.listen(port, async () => {
   await mongoose.connect(mongoDbUrl);
