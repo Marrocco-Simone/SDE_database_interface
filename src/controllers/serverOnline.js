259746bb8141594216f5ace5bf594f5aa0588c8c
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-02-16 16:15:03 +0100

    new incredible api

--- src/controllers/serverOnline.js
+++ src/controllers/serverOnline.js
@@ -1,6 +1,6 @@
 function serverOnline(req, res) {
   try {
-    res.json({ success: true });
+    res.json({ success: true, online: true });
   } catch (err) {
     console.error(err);
     res.status(500).send({ error: "Internal Server Error" });
