629784ec04d71d8fdf82a0e5835134c71b306e51
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-02-20 14:23:24 +0100

    test

--- src/controllers/serverOnline.js
+++ src/controllers/serverOnline.js
@@ -1,6 +1,6 @@
 function serverOnline(req, res) {
   try {
-    res.json({ success: true, online: true });
+    res.json({ success: true, santiago: 'love' });
   } catch (err) {
     console.error(err);
     res.status(500).send({ error: "Internal Server Error" });
