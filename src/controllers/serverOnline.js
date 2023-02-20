121a00b40088a18553c38017f59a3117937eb3f7
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-02-20 14:45:26 +0100

    working ci/cd

--- src/controllers/serverOnline.js
+++ src/controllers/serverOnline.js
@@ -1,6 +1,6 @@
 function serverOnline(req, res) {
   try {
-    res.json({ success: true, santiago: 'love' });
+    res.json({ success: true });
   } catch (err) {
     console.error(err);
     res.status(500).send({ error: "Internal Server Error" });
