6a0295b14e46aca8ffecb4448d2ec2792ee56dea
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-02-21 17:02:14 +0100

    fix typo

--- src/middleware/authenticateToken.js
+++ src/middleware/authenticateToken.js
@@ -7,11 +7,11 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) return res.status(401).json({ err: "No Token given" });
+  if (token == null) return res.status(401).json({ error: "No Token given" });
 
   jwt.verify(token, jwtSecret, (err, user) => {
     if (err) {
-      return res.status(403).json({ err: "Invalid Token" });
+      return res.status(403).json({ error: "Invalid Token" });
     }
     req.user = user;
     next();
