1143a592e862cbc7202bb4ce03e11d868a159306
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-02-23 00:53:19 +0100

    changed routes endpoints

--- src/router.js
+++ src/router.js
@@ -17,8 +17,8 @@ router.get("/", serverOnline);
 router.post("/login", login);
 router.get("/login", authenticateToken, sendTokenUser);
 router.put("/store", authenticateToken, generateNewContent);
-router.get("/get", authenticateToken, getUserContent);
-router.get("/get/:contentId", authenticateToken, getSingleContent);
-router.put("/update/:contentId", authenticateToken, updateContent);
+router.get("/content", authenticateToken, getUserContent);
+router.get("/content/:contentId", authenticateToken, getSingleContent);
+router.put("/content/:contentId", authenticateToken, updateContent);
 
 module.exports = { router };
